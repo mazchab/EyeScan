8516c8585c0557ab019cf921c264d625a04b13f3
Mazin Chabayta <mazin.chabayta@gmail.com>
2018-09-14 13:15:54 -0400

    I GOT WEBCAM ACCESS!
    figured out the problem with the p5.dom and now I have access to webcam on the quick scan page.

---
+++ quickscan.js
@@ -0,0 +1,21 @@
+<html>
+    
+<head>
+<link rel="stylesheet" type="text/css" href="futurestyle.css">
+<script src="p5.js"></script>
+</head>
+    
+    
+<body>
+function setup() {
+    createCanvas(320, 240);
+    background(51);
+    createCapture(VIDEO);
+}
+    
+function draw()
+    {
+    
+}
+</body>
+</html>
