e59e80be8740a5e3c8197a9ab878a417b706254d
Mazin Chabayta <mazin.chabayta@gmail.com>
2018-09-16 21:41:30 -0400

    quick scan & index
    Changed color of quick scan button
    
    added pathologies marks & 911 button function in quickscan.html

--- quickscan.js
+++ quickscan.js
@@ -1,24 +1,41 @@
-var canv
-var vid
-var Button
-var Green
-var mousePressed
-var img
+var canv;
+var vid;
+var Button;
+var Button2;
+var Mark;
+var pTag;
+
 
 function setup()
 {
-    canv = createCanvas(550,480);
-    canv.position(770,100);
+    canv = createCanvas(400,490);
+    canv.position(900,110);
     vid = createCapture(VIDEO);
     vid.position(120,100);
 
     Button = createButton("Capture");
     Button.position(120,590);
     Button.size(640,50);
-    Button.mouseClicked();
+    Button.mouseClicked(makeMark);
+
+
+
+
 }
 
-function mouseClicked()
+function draw()
 {
-  loadImage("FemaleBody.png")
+}
+
+function makeMark()
+{
+  fill(204,0,0);
+  ellipse(300,100,40,40);
+  ellipse(120,170,40,40);
+  createP("A list of diseases/pathologies will appear here");
+
+
+  Button2=createButton("Call 911");
+  Button2.position(1400,300);
+  Button2.size(100 , 100);
 }
